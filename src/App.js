75cc602c52d1ef4039b0310051cf07425c9f6549
RogerioNepo <rogerio@nepo.unicamp.br>
2023-05-30 15:16:22 -0300

    converting to Beer and Code

--- src/App.js
+++ src/App.js
@@ -5,17 +5,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" width="250"/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          A comunidade que todos querem estar.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://youtube.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Youtube Channel
         </a>
       </header>
     </div>
